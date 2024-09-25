b91640d7dacb35fad2591ea3b17cf26a10dfb011
Abdo0Gego0 <Abdo.gego.2000@gmail.com>
2024-09-25 17:46:13 +0400

    0.3

--- reverseSentence/reverse.cs
+++ reverseSentence/reverse.cs
@@ -7,7 +7,7 @@ class Program
         Console.WriteLine("Enter your Sentence");
         string str = Console.ReadLine();
 
-        // Reverse the order of words and output
+        // Split the string then Reverse the order of words and output then use join to concatnate into 1 output
         Console.WriteLine(string.Join(" ", str.Split(' ').Reverse()));
     }
 }
